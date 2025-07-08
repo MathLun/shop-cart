dd354e3816d090945f56fed39b4549ea8c9902a0
mathlun <matheuslunadev@gmail.com>
2025-07-08 12:32:26 -0300

    (feat) [auth_router]: check if account exists for login endpoint and register endpoint

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -22,59 +22,72 @@ export const authRouter: Router = Router()
 authRouter.post('/register', async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { username, storename, email, password, role } = req.body
-        const passwordHash = await createPasswordHash(password)
-        const isClient = role === "client"
-        const isAdmin = role === "admin"
+        const isExistsAccount = await getAccountByUsername.exec({ username: username })
+        if (isExistsAccount.data) {
+            res.json({
+                status: 301,
+                message: 'Este usuario existe, tente novamente com outro nome!'
+            })
+        } else {
+            const passwordHash = await createPasswordHash(password)
+            const isClient = role === "client"
+            const isAdmin = role === "admin"
+            const { message, data } = await createNewAccount.exec({ username, storename, email, password: passwordHash, role })
+            const tokenData = await createTokenFromAccess({ data: {
+                username: data?.getUsername(),
+                storename: data?.getStorename(),
+                email: data?.getEmail(),
+                password: data?.getPassword(),
+                role: data?.getRole()
+            }})
+            console.log("CreateNewAccount: ", message)
 
-        const { message, data } = await createNewAccount.exec({ username, storename, email, password: passwordHash, role })
-        const tokenData = await createTokenFromAccess({ data: {
-            username: data?.getUsername(),
-            storename: data?.getStorename(),
-            email: data?.getEmail(),
-            password: data?.getPassword(),
-            role: data?.getRole()
-        }})
-        console.log("CreateNewAccount: ", message)
+            const account = await getAccountByUsername.exec({ username: username })
+            const accountId = account.data?.getId()
+            console.log("GetAccountByUsername: ", account.message)
 
-        const account = await getAccountByUsername.exec({ username: username })
-        const accountId = account.data?.getId()
-        console.log("GetAccountByUsername: ", account.message)
+            if (isClient) {
+                const { message } = await createNewClient.exec({ _id: accountId, username, storename, email, password: passwordHash })
+                console.log("CreateNewClient: ", message)
+            }
 
-        if (isClient) {
-            const { message } = await createNewClient.exec({ _id: accountId, username, storename, email, password: passwordHash })
-            console.log("CreateNewClient: ", message)
-        }
+            if (isAdmin) {
+                const { message } = await createNewAdmin.exec({ _id: accountId, storename: storename, email, password: passwordHash })
+                console.log("CreateNewAdmin: ", message)
+            }
 
-        if (isAdmin) {
-            const { message } = await createNewAdmin.exec({ _id: accountId, storename: storename, email, password: passwordHash })
-            console.log("CreateNewAdmin: ", message)
-        }
-
-        res.json({
-            status: 201,
-            message: 'Conta criada com sucesso!',
-            data: { username: account.data?.getUsername(), role: account.data?.getRole(), token: tokenData.token }
-        })
+            res.json({
+                status: 201,
+                message: 'Conta criada com sucesso!',
+                data: { username: account.data?.getUsername(), role: account.data?.getRole(), token: tokenData.token }
+            })
+        }         
     } catch (error) {
         console.error('Error: ao criar a conta -> ', error)
-    }
+    }   
 })
-
 
 authRouter.post('/login', async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { username, password } = req.body
-        const account = await getAccountByUsername.exec({ username })
-        const accountPassword = account.data?.getPassword() || ''
-        const isPasswordValid = await validatePassowrdHash(accountPassword, password)
-        const tokenData = await createTokenFromAccess({ data: {
-            username: account.data?.getUsername(),
-            storename: account.data?.getStorename(),
-            email: account.data?.getEmail(),
-            password: account.data?.getPassword(),
-            role: account.data?.getRole()
-        }})
-        account && isPasswordValid && res.json({ status: 201, data: account, token: tokenData.token })
+        const isExistsAccount = await getAccountByUsername.exec({ username })
+        if (!isExistsAccount.data) {
+            res.json({
+                message: "Nome ou senha invalida, tente novamente"
+            })
+        } else {
+            const account = await getAccountByUsername.exec({ username })
+            const accountPassword = account.data?.getPassword() || ''
+            const isPasswordValid = await validatePassowrdHash(accountPassword, password)
+            const tokenData = await createTokenFromAccess({ data: {
+                username: account.data?.getUsername(),
+                storename: account.data?.getStorename(),
+                email: account.data?.getEmail(),
+                password: account.data?.getPassword(),
+                role: account.data?.getRole()
+            }})
+            account && isPasswordValid && res.json({ status: 201, data: account, token: tokenData.token })
+        }
     } catch (e) {
         console.error(e)
     }
